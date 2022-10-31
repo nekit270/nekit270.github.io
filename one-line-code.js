6b177e0eded1d3044085dea4265c66886dd143a7
nekit270 <115725325+nekit270@users.noreply.github.com>
2022-10-31 18:17:35 +0300

    Delete one-line-code.js

--- one-line-code.js
+++
@@ -1,8 +0,0 @@
-Object.prototype._set = function(obj){
-    for(let i in obj){
-        let e = obj[i];
-        this[i] = e;
-    }
-    return this;
-}
-Object.defineProperty(Object.prototype, '_set', {enumerable: false});
